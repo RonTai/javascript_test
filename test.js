4da2bf505fd19ed2555acfed132bc5bef66f40e2
name <email>
2013-04-11 00:31:26 +0900

    freedome test about 'prototype'

---
+++ test.js
@@ -0,0 +1,34 @@
+function Hoge(){
+    this.instanceVal = 99;
+    Hoge.prototype.val = 10;
+}
+"use strict"
+var a = new Hoge();
+var b = new Hoge();
+
+console.log(a.val);
+console.log(b.val);
+
+// a.valを変更し、bと比較
+a.val = 50;
+console.log(a.val);
+console.log(b.val);
+
+// prototypeを書き換えてみる
+var c = new Hoge();
+Hoge.prototype.val = 150;
+console.log(b.val);
+console.log(c.val);
+
+console.log(a instanceof Hoge);
+
+function Foo(){
+    Hoge.call(this);
+}
+
+var foo = new Foo();
+console.log(foo.prototype instanceof Hoge);
+console.log(foo.instanceVal);
+
+foo.instanceVal = 150;
+console.log(foo.instanceVal == a.instanceVal);
